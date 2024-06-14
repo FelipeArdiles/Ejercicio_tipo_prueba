959246114e7fa625a7174cea120fee824d6ac5bd
felipeArdiles <Fe.ardiles@duocuc.cl>
2024-06-14 11:22:42 -0400

    Primera funcion del menú lista, falta validar

--- ejercicios_trabajadores.py
+++ ejercicios_trabajadores.py
@@ -1,5 +1,8 @@
 import os, time
 
+
+trabajadores=[]
+
 while True:
     print("""MENÚ TRABAJADORES
           1)Registrar trabajador
@@ -9,7 +12,17 @@ while True:
     opc=int(input("Ingrese opción: "))
     os.system("cls")
     if opc==1:
-        pass
+        print("registrar trabajador").title()
+        nombre_apellido=input("Ingrese su nombre y apellido: ")
+        cargo= int(input("ingrese cargo(1:CEO, 2:DESARROLLADOR, 3:ANALISTA): "))
+        sueldo_bruto= int(input("Ingrese sueldo bruto: "))
+        descuento_salud= int(0.07*sueldo_bruto)
+        descuento_afp= int(0.12*sueldo_bruto)
+        sueldo_liquido= sueldo_bruto-descuento_afp-descuento_salud
+        trabajador=[nombre_apellido,cargo,sueldo_bruto,descuento_salud,descuento_afp,sueldo_liquido]
+        trabajadores.append(trabajador)
+        print("Trabajador registrado con éxito")
+
     elif opc==2:
         pass
     elif opc==3:
